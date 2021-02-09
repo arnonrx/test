cfd7808ee5278ecac58e480e1b9ef19f78d64cba
arnonrx <arnonrx17@gmail.com>
2021-02-09 21:51:10 +0700

    add person class

---
+++ person.py
@@ -0,0 +1,24 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    
+    def myfunc(self):
+        print('Hello my name is '+ self.name)
+
+class Persons:
+    def __init__(myObj, name, age):
+        myObj.name = name
+        myObj.age = age
+
+    def myfunc(myObj):
+        print('Hello my name is ...' + myObj.name)
+
+p1 = Person('John', 36)
+p1.myfunc()
+
+p2 = Person('Arnon', 30)
+p2.myfunc()
+
+p2.name = 'Adam'
+p2.myfunc()
